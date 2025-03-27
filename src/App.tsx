da207a108947baaf5f0441fa613b97f3b5d704e5
nabsba <ns-dev@outlook.com>
2025-03-27 20:02:53 +0000

    test(prettier + lint): applied

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> Hi my friend</h1>
+        <h1> Hi my friend</h1>
       </header>
     </div>
   );
