49d51e7aa24b721336bd9b68be129c04a65e412e
nabsba <ns-dev@outlook.com>
2025-03-28 08:30:24 +0000

    build(features/api/index.ts): update exports

--- src/features/api/index.ts
+++ src/features/api/index.ts
@@ -1,1 +1,25 @@
-export {};
+import { fetchData } from 'features/api/services/fetchData';
+import { API_ENDPOINTS } from './datas/constants';
+import {
+  ERROR_MESSAGES,
+  ERROR_MESSAGES_KEYS,
+  STATUS_SERVER_ERROR,
+} from './datas/errors';
+import {
+  SUCCESS_MESSAGES,
+  SUCCESS_MESSAGES_KEYS,
+  SUCCESS_STATUS,
+} from './datas/success';
+import { FetchResult } from './types/fetchData';
+
+export {
+  API_ENDPOINTS,
+  fetchData,
+  ERROR_MESSAGES,
+  ERROR_MESSAGES_KEYS,
+  STATUS_SERVER_ERROR,
+  SUCCESS_MESSAGES,
+  SUCCESS_MESSAGES_KEYS,
+  SUCCESS_STATUS,
+};
+export type { FetchResult };
