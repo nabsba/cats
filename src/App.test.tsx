2f4739298e0e9f6e642461be2d92bf34663f8dfe
nabsba <ns-dev@outlook.com>
2025-03-29 03:32:53 +0000

    fix(app.tsx): test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hi my friend/i);
+  const linkElement = screen.getByText(/Welcome to my cat application!/i);
   expect(linkElement).toBeInTheDocument();
 });
